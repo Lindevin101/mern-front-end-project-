49b5c17427d10605c44e182ab238ea5affa24bb0
dlin <lindevin101@gmail.com>
2023-11-25 16:55:10 -0500

    added apiconfig js

---
+++ src/services/apiConfig.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+let apiUrl;
+
+const apiUrls = {
+  production: "https://formula1-api-238f33f4ace2.herokuapp.com/api/",
+  development: "https://formula1-api-238f33f4ace2.herokuapp.com/api/",
+};
+
+if (window.location.hostname === "localhost") {
+  apiUrl = apiUrls.development;
+} else {
+  apiUrl = apiUrls.production;
+}
+
+const api = axios.create({
+  baseURL: apiUrl,
+});
+
+export default api;
