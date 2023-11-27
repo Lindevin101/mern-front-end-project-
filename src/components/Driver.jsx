23d98d211f90b4eeaf67e5142d0144eca3f755bb
dlin <lindevin101@gmail.com>
2023-11-27 12:21:25 -0500

    deploy

--- src/components/Driver.jsx
+++ src/components/Driver.jsx
@@ -4,9 +4,9 @@ export default function Driver({ driver,showModal }) {
     return (
       <div className="indiviual-driver-div">
         <div className="driver-container">
-            <h2 onClick={() => showModal(driver)}>{driver.Name}</h2>
+            <h2 onClick={() => showModal(driver)}>{driver.Name} </h2>
             <img onClick={() => showModal(driver)} className="driver-image" src={driver.image} alt={driver.Name}/>
         </div>
       </div>
     )
-  }
+  } 
